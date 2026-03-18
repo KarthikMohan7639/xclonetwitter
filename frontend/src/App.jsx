b3c879fdf77d1760613d1d3df24bffaebbb292c4
KarthikMohan7639 <karthikmohan7639@gmail.com>
2026-03-18 10:07:54 +0530

    feat: add React and Vite setup with Tailwind CSS and DaisyUI
    - Added React SVG and Vite SVG assets.
    - Created index.css for Tailwind CSS integration.
    - Set up main.jsx as the entry point for the React application.
    - Configured Tailwind CSS with DaisyUI in tailwind.config.js.
    - Established Vite configuration in vite.config.js.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-white bg-slate-400'>Welcome to the App!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+import daisyui from 'daisyui'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue,svelte}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+}
+
